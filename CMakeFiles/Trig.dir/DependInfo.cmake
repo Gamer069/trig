06cdbaf5a1551729be16ffde1605ce7615095827
Illia Zhdanov <don.izhdanov@gmail.com>
2024-07-11 00:04:36 +0300

    Initial commit (a lil broken bc imgui submodule doesnt exist atm

---
+++ CMakeFiles/Trig.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/illia/Desktop/proj/@c++/trig/lib/glad/glad.c" "CMakeFiles/Trig.dir/lib/glad/glad.c.o" "gcc" "CMakeFiles/Trig.dir/lib/glad/glad.c.o.d"
+  "/Users/illia/Desktop/proj/@c++/trig/lib/imgui/backends/imgui_impl_glfw.cpp" "CMakeFiles/Trig.dir/lib/imgui/backends/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/Trig.dir/lib/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "/Users/illia/Desktop/proj/@c++/trig/lib/imgui/backends/imgui_impl_opengl3.cpp" "CMakeFiles/Trig.dir/lib/imgui/backends/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/Trig.dir/lib/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "/Users/illia/Desktop/proj/@c++/trig/lib/imgui/imgui.cpp" "CMakeFiles/Trig.dir/lib/imgui/imgui.cpp.o" "gcc" "CMakeFiles/Trig.dir/lib/imgui/imgui.cpp.o.d"
+  "/Users/illia/Desktop/proj/@c++/trig/lib/imgui/imgui_demo.cpp" "CMakeFiles/Trig.dir/lib/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/Trig.dir/lib/imgui/imgui_demo.cpp.o.d"
+  "/Users/illia/Desktop/proj/@c++/trig/lib/imgui/imgui_draw.cpp" "CMakeFiles/Trig.dir/lib/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/Trig.dir/lib/imgui/imgui_draw.cpp.o.d"
+  "/Users/illia/Desktop/proj/@c++/trig/lib/imgui/imgui_tables.cpp" "CMakeFiles/Trig.dir/lib/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/Trig.dir/lib/imgui/imgui_tables.cpp.o.d"
+  "/Users/illia/Desktop/proj/@c++/trig/lib/imgui/imgui_widgets.cpp" "CMakeFiles/Trig.dir/lib/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/Trig.dir/lib/imgui/imgui_widgets.cpp.o.d"
+  "/Users/illia/Desktop/proj/@c++/trig/src/main.cc" "CMakeFiles/Trig.dir/src/main.cc.o" "gcc" "CMakeFiles/Trig.dir/src/main.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Trig.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Trig.
+# This may be replaced when dependencies are built.
